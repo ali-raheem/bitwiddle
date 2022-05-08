141acf8abf15416c780f6f09dc564d5497af7e24
Ali Raheem <ali-windows@shoryuken.me>
2022-05-08 10:28:11 +0100

    Fix typo

--- bitwiddle.v
+++ bitwiddle.v
@@ -2,23 +2,23 @@ module bitwiddle
 
 // u64_from_be_u8 convert a big endian array containing 8 u8s into a u64
 pub fn u64_from_be_u8(a []u8) u64 {
-	return u32(a[0]) << 56 | u32(a[1]) << 48 | u32(a[2]) << 40 | u32(a[3]) << 32 | u32(a[4]) << 24 | u32(a[5]) << 16 | u32(a[6]) << 8 | u32(a[7])
+	return u64(a[0]) << 56 | u64(a[1]) << 48 | u64(a[2]) << 40 | u64(a[3]) << 32 | u64(a[4]) << 24 | u64(a[5]) << 16 | u64(a[6]) << 8 | u64(a[7])
 }
 
 // u64_from_le_u8 convert a little endian array containing 8 u8s into a u64
-pub fn u64_from_le_u8(a []u8) u36 {
-	return u32(a[0]) | u32(a[1]) << 8 | u32(a[2]) << 16 | u32(a[3]) << 24 | u32(a[4]) << 32 | u32(a[5]) << 40 | u32(a[6]) << 48 | u32(a[7]) << 56
+pub fn u64_from_le_u8(a []u8) u64 {
+	return u64(a[0]) | u64(a[1]) << 8 | u64(a[2]) << 16 | u64(a[3]) << 24 | u64(a[4]) << 32 | u64(a[5]) << 40 | u64(a[6]) << 48 | u64(a[7]) << 56
 }
 
 // u64_to_be_u8 convert an u64 to a big endian array containing 8 u8s
 pub fn u64_to_be_u8(a u64) []u8 {
-	return [u8(a >> 56), u8(a >> 48), u8(a >> 40), u8(a) >> 32, u8(a >> 24), u8(a >> 16), u8(a >> 8),
+	return [u8(a >> 56), u8(a >> 48), u8(a >> 40), u8(a >> 32), u8(a >> 24), u8(a >> 16), u8(a >> 8),
 		u8(a)]
 }
 
 // u64_to_le_u8 convert an u64 to a little endian array containing 8 u8s
 pub fn u64_to_le_u8(a u64) []u8 {
-	return [u8(a), u8(a >> 8), u8(a >> 16), u8(a >> 24), u8(a) >> 32, u8(a >> 40), u8(a >> 48),
+	return [u8(a), u8(a >> 8), u8(a >> 16), u8(a >> 24), u8(a >> 32), u8(a >> 40), u8(a >> 48),
 		u8(a >> 56)]
 }
 
